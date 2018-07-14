fb9d6835587a11bcfdd5571ace801874b7372db6
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2018-07-14 15:51:21 +0800

    Can only handle one task - needs refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -40,17 +40,22 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('tasks-table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Buy a new laptop' for row in rows),
-            msg="The new task didn't appear in the table."
-        )
-
-        self.fail('Unfinished test...')
+        self.assertIn('1. Buy a new laptop', [row.text for row in rows])
 
         # There is still a text box inviting him to add another task.
         # He enters "Buy a new keyboard"
+        input_box = self.browser.find_element_by_id('new-task')
+        input_box.send_keys('Buy a new keyboard')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, showing both of the tasks
+        table = self.browser.find_element_by_id('tasks-table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1. Buy a new laptop', [row.text for row in rows])
+        self.assertIn('2. Buy a new keyboard', [row.text for row in rows])
+
+        self.fail('Unfinished test...')
 
         # John wonders if the site will remember his tasks.
         # Then he sees that the site generated a unique URL for him.
--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'title': 'A new task item'})
+        self.assertIn('A new task item', response.content.decode())
+        self.assertTemplateUsed(response, 'lists/home.html')
