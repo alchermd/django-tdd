ded3f4c487e2596d99a0f49f088876ffa3dc87be
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2018-07-14 17:40:42 +0800

    Use the Django LiveServerTestCase instead of the one from the unittest module

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
 import time
-import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -16,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # John heard about a new task manager app.
         # He checks out its home page.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the page title and header mentions about tasks.
         self.assertIn('Task', self.browser.title)
@@ -62,7 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('tasks-table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-
-
-if __name__ == '__main__':
-    unittest.main()
